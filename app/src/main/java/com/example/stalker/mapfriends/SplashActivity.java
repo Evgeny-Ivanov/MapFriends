77b769b4650783fa869fe4d06518f05abe05cc8f
evgeny-ivanov <Saboteuur@yandex.ru>
2016-05-21 19:53:50 +0300

    ничего не сделано

--- app/src/main/java/com/example/stalker/mapfriends/SplashActivity.java
+++ app/src/main/java/com/example/stalker/mapfriends/SplashActivity.java
@@ -3,14 +3,20 @@ package com.example.stalker.mapfriends;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 
 import com.example.stalker.mapfriends.coordinatesSave.CoordinatesSaveService;
+import com.mikepenz.materialdrawer.util.AbstractDrawerImageLoader;
+import com.mikepenz.materialdrawer.util.DrawerImageLoader;
+import com.squareup.picasso.Picasso;
 import com.vk.sdk.util.VKUtil;
 
 //тут проверяем авторизацию
@@ -38,5 +44,28 @@ public class SplashActivity extends AppCompatActivity {
             }
         }, timeDelayed);
 
+        DrawerImageLoader.init(abstractDrawerImageLoader);//определяем как библиотека будет скачивать изображения
     }
+
+    private AbstractDrawerImageLoader abstractDrawerImageLoader = new AbstractDrawerImageLoader() {
+        @Override
+        public void set(ImageView imageView, Uri uri, Drawable placeholder) {
+            super.set(imageView, uri, placeholder);
+            Picasso
+                    .with(imageView.getContext())
+                    .load(uri).placeholder(placeholder)
+                    .error(R.drawable.img_default)
+                    .into(imageView);
+        }
+
+        @Override
+        public void cancel(ImageView imageView) {
+            super.cancel(imageView);
+            Picasso
+                    .with(imageView.getContext())
+                    .cancelRequest(imageView);
+        }
+    };
+
+
 }
--- app/src/main/java/com/example/stalker/mapfriends/network/APIServerCoor.java
+++ app/src/main/java/com/example/stalker/mapfriends/network/APIServerCoor.java
@@ -1,6 +1,8 @@
 package com.example.stalker.mapfriends.network;
 
+import com.example.stalker.mapfriends.message.DataAndStatusMsg;
 import com.example.stalker.mapfriends.message.DataMsg;
+import com.example.stalker.mapfriends.message.RequestDataMsg;
 import com.example.stalker.mapfriends.message.StatusMsg;
 
 import retrofit2.Call;
@@ -10,7 +12,12 @@ import retrofit2.http.POST;
 /**
  * Created by stalker on 16.04.16.
  */
-public interface APIServerSendCoor {
+public interface APIServerCoor {
+    String SERVER_URL = "http://195.19.44.134:8081/";
+
     @POST("data-to-server")
     Call<StatusMsg> send(@Body DataMsg msg);
+
+    @POST("data-from-server")
+    Call<DataAndStatusMsg> get(@Body RequestDataMsg msg);
 }
